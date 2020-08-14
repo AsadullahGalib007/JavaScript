11ee91ba26f45e08590519b247ae0a3f88c0c0db
AsadullahGalib007 <abrgalib@gmail.com>
2020-08-15 01:11:35 +0600

    ---

--- HTML, CSS, and JavaScript for Web Dev/event/script.js
+++ HTML, CSS, and JavaScript for Web Dev/event/script.js
@@ -1,15 +1,26 @@
 document.addEventListener("DOMContentLoaded",
 	function (event) {
 		function sayHello(event) {
-			console.log(this);
+			// console.log(this);
+			// console.log(event);
 			this.textContent = "Said it";
 			var name = document.getElementById('name').value;
 			x = document.getElementById('content');
 			var msg = "<h2>Hello " + name+ "!</h2>"
 			x.innerHTML = msg;
 		}
+		
 		// Unobtrusive event binding
 		document.querySelector("button").addEventListener("click", sayHello);
+
+		document.querySelector("body").addEventListener("mousemove",
+			function (event) {
+				// document.write(event.clientX);
+				// document.write(event.clientY);
+				var y = document.getElementById("coordinate");
+				y.innerHTML = event.clientX + " , " + event.clientY;
+			}
+		);
 	}
 );
 
